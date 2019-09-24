b4614d713ada95fba539136739d4378d512ea3e8
twilightscapes <ihatespam@gmail.com>
2019-09-24 10:27:27 -0500

    add animation

--- src/templates/capabilities-page.js
+++ src/templates/capabilities-page.js
@@ -3,6 +3,7 @@ import PropTypes from 'prop-types'
 import { graphql } from 'gatsby'
 import Layout from '../components/Layout'
 import Content, { HTMLContent } from '../components/Content'
+import ScrollAnimation from 'react-animate-on-scroll';
 
 
 export const CapabilitiesPageTemplate = ({ title, content, contentComponent }) => {
@@ -11,18 +12,25 @@ export const CapabilitiesPageTemplate = ({ title, content, contentComponent }) =
 
   return (
     <section className="outer section">
+    
+    <ScrollAnimation animateIn="moveRight" animateOnce={true}>
       <div className="container">
- 
               <h2 className="title is-size-3 has-text-weight-bold is-bold-light">
                 {title}
               </h2>
               <PageContent className="content" content={content} />
-              
-              
-  
-
-   
-      </div>
+       </div>
+       </ScrollAnimation>
+       
+       <ScrollAnimation animateIn="moveLeft" animateOnce={true}>
+       <div className="container">
+              <h2 className="title is-size-3 has-text-weight-bold is-bold-light">
+                BooyA!
+              </h2>
+              <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industrys standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.</p> 
+       </div>
+       </ScrollAnimation>
+       
     </section>
   )
 }

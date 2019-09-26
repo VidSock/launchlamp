bfe6565bc81b0b36fee1d02e295ec9dadf502704
twilightscapes <ihatespam@gmail.com>
2019-09-26 08:49:56 -0500

    add example page to cms preview

--- src/cms/cms.js
+++ src/cms/cms.js
@@ -6,12 +6,14 @@ import CapabilitiesPagePreview from './preview-templates/CapabilitiesPagePreview
 import AboutPagePreview from './preview-templates/AboutPagePreview'
 import BlogPostPreview from './preview-templates/BlogPostPreview'
 import ProductPagePreview from './preview-templates/ProductPagePreview'
+import ExamplePagePreview from './preview-templates/ExamplePagePreview'
 import IndexPagePreview from './preview-templates/IndexPagePreview'
 
 CMS.registerMediaLibrary(uploadcare);
 CMS.registerMediaLibrary(cloudinary);
 
 CMS.registerPreviewTemplate('index', IndexPagePreview)
+CMS.registerPreviewTemplate('example', ExamplePagePreview)
 CMS.registerPreviewTemplate('about', AboutPagePreview)
 CMS.registerPreviewTemplate('capabilities', CapabilitiesPagePreview)
 CMS.registerPreviewTemplate('products', ProductPagePreview)

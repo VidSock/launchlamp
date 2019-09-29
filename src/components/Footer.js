3d6858d0e0ae58dcd6b868b0f125053e01531591
twilightscapes <ihatespam@gmail.com>
2019-09-29 14:29:47 -0500

    Update Footer.js

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -9,6 +9,9 @@ import twitter from '../img/social/twitter.svg'
 import vimeo from '../img/social/vimeo.svg'
 */
 
+
+
+
 const Footer = class extends React.Component {
   render() {
     return (
@@ -17,12 +20,17 @@ const Footer = class extends React.Component {
           
 
 <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industrys standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum. </p>
-<ul className="socialmenu">
+
+<ul className="socialmenu" style={{textAlign:'center', justifyContent:'center', margin:'3rem 0'}}>
   <li><h3 className="socialtext">We're<br />Social:</h3></li>
 <li><a className="social" href="https://facebook.com"><FaFacebookSquare /></a></li>
 <li><a className="social" href="https://linkedin.com"><FaLinkedin /></a></li>
 <li><a className="social" href="https://twitter.com"><FaTwitterSquare /></a></li>
-</ul>      
+</ul>
+
+
+
+<div style={{textAlign: 'center', justifyContent: 'center'}}>Copyright &copy; {(new Date().getFullYear())} Hytron Manufacturing Inc.</div>
         </div>
       </footer>
     )
@@ -30,3 +38,4 @@ const Footer = class extends React.Component {
 }
 
 export default Footer
+

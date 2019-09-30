b613e47804271218af1e3695a1be839cb5e1030d
twilightscapes <ihatespam@gmail.com>
2019-09-30 17:59:52 -0500

    fix for accessibility score

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -23,9 +23,9 @@ const Footer = class extends React.Component {
 
 <ul className="socialmenu" style={{textAlign:'center', justifyContent:'center', margin:'3rem 0'}}>
   <li><h3 className="socialtext">We're<br />Social:</h3></li>
-<li><a className="social"  href="https://facebook.com"><FaFacebookSquare /></a></li>
-<li><a className="social" href="https://linkedin.com"><FaLinkedin /></a></li>
-<li><a className="social" href="https://twitter.com"><FaTwitterSquare /></a></li>
+<li><a className="social"  href="https://facebook.com"><FaFacebookSquare /><span>Facebook</span></a></li>
+<li><a className="social" href="https://linkedin.com"><FaLinkedin /><span>LinkedIn</span></a></li>
+<li><a className="social" href="https://twitter.com"><FaTwitterSquare /><span>Twitter</span></a></li>
 </ul>
 
 

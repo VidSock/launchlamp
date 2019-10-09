70f0f8ce18af9543c21ed1a51e31e0b6006528a4
twilightscapes <ihatespam@gmail.com>
2019-10-08 22:29:52 -0500

    Update index-page.js

--- src/templates/index-page.js
+++ src/templates/index-page.js
@@ -1,12 +1,12 @@
 import React from 'react'
 import PropTypes from 'prop-types'
-// import { Link, graphql } from 'gatsby'
+import { graphql } from 'gatsby'
 
 import Layout from '../components/Layout'
 import Features from '../components/Features'
 // import BlogRoll from '../components/BlogRoll'
-
-
+import ScrollAnimation from 'react-animate-on-scroll'
+import { IoIosAperture } from 'react-icons/io'
 
 export const IndexPageTemplate = ({
   image,
@@ -17,85 +17,132 @@ export const IndexPageTemplate = ({
   description,
   intro,
 }) => (
-	
+  
   <div className="outer intro">
+  
+  
+  
     <div
       className="full-width-image margin-top-0"
-      style={{
+      style={{display: 'block',
+	      position: 'relative',
         backgroundImage: `url(${
           !!image.childImageSharp ? image.childImageSharp.fluid.src : image
         })`,
-        backgroundPosition: `top left`,
-        
+        backgroundPosition: `bottom right`,
+        width: '100%',
         backgroundSize: 'cover',
-        height: '100vh',
+        height: '90vh',
+        
       }}
     >
-      <div
-        style={{
-          display: 'flex',
-          height: '350px',
+    
+    <div style={{
+          display: 'block',
           lineHeight: '1',
-//           justifyContent: 'space-around',
-          alignItems: 'left',
           flexDirection: 'column',
+          padding: '2rem 0',
         }}
+        className="media"
       >
+      
+        <ScrollAnimation animateIn="bounceInDown" delay={100}>
         <h1
-          className="has-text-weight-bold is-size-3-mobile is-size-2-tablet is-size-1-widescreen"
+          className="normal txtshadow-header mobile-txt"
           style={{
-            lineHeight: '1',
             color: '#fff',
+            fontSize: '5rem',
+           position: 'relative',
+//            top: '100px',
+           right: '3%',
+//            backgroundColor: '#ff0000',
+            textAlign: 'right',  
           }}
         >
-          {title}
+          Follow me
         </h1>
+        </ScrollAnimation>
+        
+        <ScrollAnimation animateIn="bounceInRight">
         <h3
-          className="has-text-weight-color is-size-5-mobile is-size-5-tablet is-size-4-widescreen"
+          className="narrow txtshadow mobile-txt"
           style={{
-           
+           fontSize:'3rem',
+           color: 'white',
+           position: 'relative',
+        right: '5%',
+            textAlign: 'right', 
           }}
         >
-          {subheading}
+          into the
         </h3>
-      </div>
-    </div>
-    <section className="section section--gradient">
-      <div className="container">
-        <div className="section">
-          <div className="columns">
-            <div className="column is-10 is-offset-1">
-              <div className="content">
-                <div className="content">
-                  <div className="tile">
-                    <h1 className="title">{mainpitch.title}</h1>
-                  </div>
-                  <div className="tile">
-                    <h3 className="subtitle">{mainpitch.description}</h3>
-                  </div>
-                </div>
-                <div className="columns">
-                  <div className="column is-12">
-                    <h3 className="has-text-weight-semibold is-size-2">
-                      {heading}
-                    </h3>
-                    <p>{description}</p>
-                  </div>
-                </div>
-                
-                
-                <Features gridItems={intro.blurbs} />
-                
-                
-               
-                
-                
-              </div>
-            </div>
-          </div>
+        </ScrollAnimation>
+        
+        
+        <ScrollAnimation animateIn="bounceInUp">
+        <h3
+          className="black txtshadow-3d mobile-txt"
+          style={{
+           fontSize:'7rem',
+           color: 'white',
+           position: 'relative',
+//            top: '230px',
+           right: '3%',
+            textAlign: 'right', 
+          }}
+        >
+          Night
+        </h3>
+        </ScrollAnimation>
+        
+        
+        
+        <ScrollAnimation animateIn="fadeIn" delay={1000} animateOnce={false}>
+        <a href="#contact">
+        <button style={{
+           position: 'absolute',
+           margin: '0 auto',
+		   top: '60vh',
+           right: '3%',
+           color: 'white',
+           background: '#1257a2',
+           padding: '.5rem 1rem .5rem 3rem',
+           borderRadius: '8px',
+           fontSize: '170%',
+//            textTransform: 'uppercase',
+           cursor: 'pointer',
+           animation: 'fadeIn 3s',
+          }}>
+          <IoIosAperture style={{position:'absolute', left:'5%', top:'8px', fontSize: '110%',}} />Contact Todd</button></a>
+        </ScrollAnimation>
+        
+        
+        
+        
+        
         </div>
       </div>
+      
+      
+      
+      
+      
+    
+    
+    <section className="section section--gradient">
+      <div className="container">
+     
+                <Features gridItems={intro.blurbs} />
+
+      </div>
     </section>
+    
+    
+                      
+                    
+                 
+                 
+                
   </div>
 )
 
@@ -111,15 +158,10 @@ IndexPageTemplate.propTypes = {
   }),
 }
 
-
-
 const IndexPage = ({ data }) => {
   const { frontmatter } = data.markdownRemark
 
   return (
-	  
-	  
-	  
     <Layout>
       <IndexPageTemplate
         image={frontmatter.image}
@@ -151,7 +193,7 @@ export const pageQuery = graphql`
         title
         image {
           childImageSharp {
-            fluid(maxWidth: 1600, quality: 64) {
+            fluid(maxWidth: 1800, quality: 64) {
               ...GatsbyImageSharpFluid_withWebp
             }
           }
@@ -167,7 +209,7 @@ export const pageQuery = graphql`
           blurbs {
             image {
               childImageSharp {
-                fluid(maxWidth: 480, quality: 64) {
+                fluid(maxWidth: 540, quality: 44) {
                   ...GatsbyImageSharpFluid_withWebp
                 }
               }

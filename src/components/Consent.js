3f56e5a819ea74c19d51e211e3dd7edb1488bf9a
twilightscapes <ihatespam@gmail.com>
2019-11-02 17:17:37 -0500

    Update Consent.js

--- src/components/Consent.js
+++ src/components/Consent.js
@@ -12,21 +12,21 @@ const Consent = () => (
 	debug={true}
 	location="bottom"
 	style={{ background: "#222", zIndex:'0', }}
-	enableDeclineButton={true}
+	enableDeclineButton={false}
 	declineButtonText="No Cookies"
     declineButtonStyle={{ color: "#fff", background: "#666", fontSize: "13px" }}
     
-    buttonText="Cookies are fine"
+    buttonText="Accept"
 	buttonStyle={{ color: "#fff", background: "#FF0000", fontSize: "13px" }}
 
     expires={30}
     cookieName="Hytron.com-site-pref-cookie"
 >
 
-    <div style={{padding:'0 0 0 1rem', fontSize:'90%',}}>This website uses cookies to enhance the user experience.{" "}
+    <div style={{padding:'0 0 0 1rem', fontSize:'90%',}}> We use cookies to optimize our website and our service.{" "}<br />
     <span style={{ fontSize: "80%" }}>
-    <a href="/privacy/" style={{color:'#fff',}}>[ privacy policy ]</a>
-    </span></div>
+   <a href="/privacy/" style={{color:'#fff',}}>Do Not Sell My Personal Information</a> | <a href="/privacy/" style={{color:'#fff',}}>Privacy Statement</a>
+    </span><br /><br /></div>
     
 </CookieConsent>
 
